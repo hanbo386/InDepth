2f68db2b4c7a2ea5d16f5f64cb11d804a7a87cca
bhan <bo.han@trademe.co.nz>
2019-05-13 12:22:34 +1200

    create new project

---
+++ InDepth/Chapters/Chapter2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InDepth.Chapters
+{
+    class Chapter2
+    {
+        public void TestTypeOf<T>()
+        {
+            Console.WriteLine(typeof(T));
+            Console.WriteLine(typeof(List<>));
+            Console.WriteLine(typeof(List<T>));
+            Console.WriteLine(typeof(List<int>));
+        }
+    }
+
+    
+}
---
+++ InDepth/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InDepth.Chapters;
+
+namespace InDepth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cpt2 = new Chapter2();
+            cpt2.TestTypeOf<int>();// type inference 
+        }
+    }
+}
